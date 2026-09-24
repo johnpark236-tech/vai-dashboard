79049123304631906854bd7fb3f7881f7ba0c5ab
johnpark236 <johnpark236@gmail.com>
2026-09-25 00:00:10 +0900

    fix(config): update cloudflare tunnel url to minor-blvd-continent-studies

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.VAI_CONFIG = {
-  apiBase: "https://natural-textbooks-ian-gmt.trycloudflare.com",
+﻿window.VAI_CONFIG = {
+  apiBase: "https://minor-blvd-continent-studies.trycloudflare.com",
   googleClientId: "363284724091-lc2hnng8rodo7tds22rp2j7itbughski.apps.googleusercontent.com",
   refreshMs: 10000
 };
